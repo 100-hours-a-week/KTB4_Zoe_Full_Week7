0a9730f8a6c4da57b0df8c34c78cfe22929ea76b
yereong <lily030624@gmail.com>
2026-07-23 21:25:00 +0900

    feat: diffing 함수 추가

--- js/lib/vdom.js
+++ js/lib/vdom.js
@@ -31,3 +31,64 @@ function createElement(vNode) {
 export function render(vNode, container) {
     container.replaceChildren(createElement(vNode));
 }
+
+export function updateElement (parent, newNode, oldNode, index=0) {
+
+    if (!newNode && oldNode) {
+        return parent.removeChild(parent.childNodes[index]);
+    }
+
+    if (newNode && !oldNode) {
+        return parent.appendChild(createElement(newNode));
+    }
+
+    if (typeof newNode === "string" && typeof oldNode === "string") {
+        if (newNode === oldNode) return;
+        return parent.replaceChild(
+            createElement(newNode),
+            parent.childNodes[index]
+        )
+    }
+
+    if (typeof newNode !== typeof oldNode) {
+        return parent.replaceChild(
+            createElement(newNode),
+            parent.childNodes[index]
+        )
+    }
+
+    updateAttributes(
+        parent.childNodes[index],
+        newNode.props || {},
+        oldNode.props || {}
+    );
+
+    const maxLength = Math.max(
+        newNode.children.length,
+        oldNode.children.length
+    );
+
+    for (let i=0; i<maxLength; i++) {
+        updateElement(
+            parent.childNodes[index],
+            newNode.children[i],
+            oldNode.children[i]
+        );
+    }
+
+
+
+}
+
+function updateAttributes(target, newProps, oldProps) {
+    for ([attribute, value] of Object.entries(newProps)) {
+        if (oldProps[attribute] === newProps[attribute]) continue;
+        target.setAttribute(attribute,value);
+    }
+
+    for ([attribute,value] of Object.entries(newProps)) {
+        if (newProps[attribute] !== undefined) continue;
+        target.removeAttribute(attribute);
+    }
+
+}
